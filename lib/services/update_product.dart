b37db46a0ae3a69dbd77a83e7ad35b699d15a303
FiloHany <feloh64@gmail.com>
2025-10-26 01:03:55 +0300

    Put Method in api helper

---
+++ lib/services/update_product.dart
@@ -0,0 +1,25 @@
+import 'package:store/helper/api.dart';
+import 'package:store/models/product_model.dart';
+
+class UpdateProductServices{
+
+  Future<ProductModel> addProduct (
+      {required String title,
+      required String price,
+      required String description,
+      required String image,
+      required String category}
+  ) async{
+    Map<String, dynamic> data = await API().put(
+        url: 'https://fakestoreapi.com/products',
+        body: {
+          'title': title,
+          'price': price,
+          'description': description,
+          'image': image,
+          'category': category,
+        }
+    );
+    return ProductModel.fromJson(data);
+  }
+}
